a051139e85859d88bcde1934a273be4b176805a0
Ronel David <ronel05032@gmail.com>
2020-05-24 14:21:54 +0300

    pls work

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<open-workout-web>'));
+app.use(express.static('./dist/<name-on-package.json>'));
 
-app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<open-workout-web>/index.html'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+);
 });
 
 // Start the app by listening on the default Heroku port
