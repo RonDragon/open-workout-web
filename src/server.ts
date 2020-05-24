20e2120a851dc15510d863e638c3b95d83935999
Ronel David <ronel05032@gmail.com>
2020-05-24 15:06:15 +0300

    test again

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import * as express from "express";
+
+class Server {
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+  }
+  // Serve only the static files form the dist directory
+
+  public config(): void {
+    const app = this.app;
+    app.use(express.static("./dist/<open-workout-web>"));
+    app.listen(process.env.PORT || 8080);
+    app.get("/*", function (req, res) {
+      res.sendFile("index.html", { root: "dist/<open-workout-web>/" });
+    });
+  }
+
+}
