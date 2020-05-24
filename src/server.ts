ddf119b8990fc0e613742c03d5b5beb2120b121d
Ronel David <ronel05032@gmail.com>
2020-05-24 15:13:01 +0300

    t

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ class Server {
   public config(): void {
     const app = this.app;
     app.use(express.static("./dist/<open-workout-web>"));
-    app.listen(process.env.PORT || 8080);
+    app.listen("port",process.env.PORT || 8080);
     app.get("/*", function (req, res) {
       res.sendFile("index.html", { root: "dist/<open-workout-web>/" });
     });
